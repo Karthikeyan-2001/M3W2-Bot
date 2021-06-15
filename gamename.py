22bbd578ace5927818dca11d885f8e7c6edae08a
Karthikeyan J <60818223+Karthikeyan-2001@users.noreply.github.com>
2021-06-15 05:06:28 +0000

    Voice teamer command

---
+++ gamename.py
@@ -0,0 +1,27 @@
+games=['Doom Eternal', 'Destiny 2', 'Call of Duty: Warzone', 'Half-Life: Alyx', 'Halo: The Master Chief Collection', 'Tom Clancy’s Rainbow Six Siege', 'Titanfall 2', 'Black Mesa', 'Apex Legends', 'Superhot', 'Battlefield 5', 'Dusk', 'Deep Rock Galactic', 'Bulletstorm', 'Escape from Tarkov', 'Borderlands 3', 'PlayerUnknown\'s Battlegrounds', 'Wolfenstein 2: New Colossus', 'Overwatch', 'Metro Exodus', 'Payday 2', 'F.E.A.R', 'Call of Duty: Modern Warfare Remastered', 'Team Fortress 2', 'Bioshock Infinite', 'Half-Life 2', 'Left 4 Dead 2', 'Insurgency: Sandstorm', 'Valorant', 'Counter-Strike: Global Offensive', 'Enlisted'
+]
+
+
+gifs2=['https://i.gifer.com/L9sl.gif', 
+'https://thumbs.gfycat.com/FairEnormousAfricanbushviper-size_restricted.gif',
+'https://thumbs.gfycat.com/CandidWholeAtlanticsharpnosepuffer-size_restricted.gif',
+'https://i.gifer.com/JrmE.gif',
+'http://38.media.tumblr.com/3bfc157ca2f5358cc0f263dd5e676593/tumblr_na0vrrz12p1toiaw4o2_500.gif',
+'https://i.gifer.com/G9CB.gif',
+'https://i.pinimg.com/originals/b5/ca/da/b5cada20a67112e4baa6126c051f3506.gif',
+'http://25.media.tumblr.com/618ecd300cf043c09fe339d4a56239d3/tumblr_muhcuwrAmD1sq3bjlo1_500.gif',
+'https://giffiles.alphacoders.com/520/5209.gif',
+'https://i.gifer.com/BXqy.gif',
+'https://i.pinimg.com/originals/c1/72/63/c172635bd1dedb9db1d0fbbe44d1d334.gif',
+'http://31.media.tumblr.com/2c0fa16a58b5d065c22db629a80d3b10/tumblr_mr33b0syeG1r8sc3ro1_1280.gif',
+'http://i.imgur.com/trH34.gif',
+'https://i.gifer.com/EcaK.gif',
+'http://31.media.tumblr.com/83370030a8e54c9b6c1753997531f749/tumblr_n6idbwwnGJ1rpn9eno1_500.gif',
+'http://38.media.tumblr.com/547f65f16965bc1745200c0e7cc732a8/tumblr_ne2t12QKF41s8sotmo2_500.gif',
+'https://64.media.tumblr.com/92dc9f1fa2483285619df09226f6ddb2/b37e9cd6b613d87f-5b/s640x960/a86636d33f5975ebbe9d989d97cb59ce7e4b4dd8.gif',
+'https://i.chzbgr.com/full/7515128320/h6D89A43C/dont-mess-with-mewtwo',
+'https://i.gifer.com/G9CB.gif',
+'http://31.media.tumblr.com/8d164f2c6ea1974aa9eb4ce0ae50a5a4/tumblr_ncgdbrMn951toiaw4o1_500.gif',
+'https://media.giphy.com/media/Ifrxg7DlJlSkNuaoeT/giphy.gif',
+
+]
--- main.py
+++ main.py
@@ -1,95 +1,187 @@
 import discord
 import os
-import requests
+import numpy as np
 import json
 import random
-from replit import db
+from discord.ext import commands, tasks
 from keep_alive import keep_alive
+from itertools import cycle
+from gamename import games, gifs2
 
-client = discord.Client()
+#Prefix
+def get_prefix(client,message):
+  with open('prefixes.json','r') as f:
+    prefixes=json.load(f)
 
-sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
+  return prefixes[str(message.guild.id)]
 
-starter_encouragements = [
-  "Cheer up!",
-  "Hang in there.",
-  "You are a great person / bot!"
-]
+#Initializing Client
+client = commands.Bot(command_prefix = get_prefix, help_command=None)
 
-if "responding" not in db.keys():
-  db["responding"] = True
+status=cycle(games)
 
-def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return(quote)
+#Prefix on server join
+@client.event
+async def on_guild_join(guild):
+  with open('prefixes.json','r') as f:
+    prefixes=json.load(f)
 
-def update_encouragements(encouraging_message):
-  if "encouragements" in db.keys():
-    encouragements = db["encouragements"]
-    encouragements.append(encouraging_message)
-    db["encouragements"] = encouragements
-  else:
-    db["encouragements"] = [encouraging_message]
+  prefixes[str(guild.id)] = '+'
 
-def delete_encouragment(index):
-  encouragements = db["encouragements"]
-  if len(encouragements) > index:
-    del encouragements[index]
-    db["encouragements"] = encouragements
+  with open('prefixes.json', 'w') as f:
+    json.dump(prefixes,f, indent=4)
 
+#Prefix delete on leaving server
+@client.event
+async def on_guild_remove(guild):
+  with open('prefixes.json','r') as f:
+    prefixes=json.load(f)
+
+  prefixes.pop(str(guild.id))
+
+  with open('prefixes.json', 'w') as f:
+    json.dump(prefixes,f, indent=4)
+
+
+#Client ready
 @client.event
 async def on_ready():
+  change_status.start()
   print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
+
+#Change Status
+@tasks.loop(minutes=2)
+async def change_status():
+  await client.change_presence(activity = discord.Game(next(status)))
+
+#Change Prefix
+@client.command()
+async def changeprefix(ctx, prefix):
+  with open('prefixes.json','r') as f:
+    prefixes=json.load(f)
+
+  prefixes[str(ctx.guild.id)] = prefix
+
+  with open('prefixes.json', 'w') as f:
+    json.dump(prefixes,f, indent=4)
+
+  await ctx.send(f'prefix changed to {prefix}')
+
+#Voice team maker
+@client.command()
+async def vteam(ctx,notm):
+
+  j,k=0,0
+  notm = int(notm)
+  cate1 = 'M3W2 Teams'
+  chan1 = 'M3W2 Lobby'
+  chn=[]
+  memlist=list()
+  a=ctx.author.id
+  for i in ctx.guild.categories:
+    if cate1 == i.name:
+      k=1
+      break
+  if k==0:
+    await ctx.guild.create_category('M3W2 Teams')
+    await ctx.send('Created channel successfully..!')
+
+  for i in ctx.guild.channels:
+    if chan1== i.name:
+      j=1
+      break
+  cat = discord.utils.get(ctx.guild.categories, name='M3W2 Teams')
+  if j==0:
+    await ctx.guild.create_voice_channel(chan1, category=cat, user_limit=None)
+    await ctx.send('Join M3W2 Lobby channel and resend the command..!')
     return
 
-  msg = message.content
+  voice_channel = discord.utils.get(ctx.guild.channels, name="M3W2 Lobby")
 
-  if msg.startswith('$inspire'):
-    quote = get_quote()
-    await message.channel.send(quote)
+  await voice_channel.edit(category=cat)
 
-  if db["responding"]:
-    options = starter_encouragements
-    if "encouragements" in db.keys():
-      options = options + db["encouragements"]
+  for c in ctx.guild.voice_channels:
+    if str(c).startswith('M3W2 Team - '):
+      memid = c.voice_states.keys()
+      memlist=list(memid)
+      if len(memlist) == 0:
+        await c.delete()
 
-    if any(word in msg for word in sad_words):
-      await message.channel.send(random.choice(options))
 
-  if msg.startswith("$new"):
-    encouraging_message = msg.split("$new ",1)[1]
-    update_encouragements(encouraging_message)
-    await message.channel.send("New encouraging message added.")
+  memid= voice_channel.voice_states.keys()
+  memlist=list(memid)
 
-  if msg.startswith("$del"):
-    encouragements = []
-    if "encouragements" in db.keys():
-      index = int(msg.split("$del",1)[1])
-      delete_encouragment(index)
-      encouragements = db["encouragements"]
-    await message.channel.send(encouragements)
+  if a not in memlist:
+    await ctx.send('Join M3W2 Lobby channel and resend the command..!')
+    return
+  
+  if notm>len(memlist):
+    await ctx.send('Specify No. of teams less than members..!')
+    return
+  
+  random.shuffle(memlist)
+  memlist= np.array_split(memlist, notm)
+  await ctx.send(type(int(memlist[0][0])))
+  for i in range(0,notm):
+    chanT = 'M3W2 Team - '+str(i+1)
+    chn = await ctx.guild.create_voice_channel(chanT, category=cat, user_limit=memlist[i].size)
+    await ctx.send(chn)
+    ids=memlist[i]
+    for j in range(0,len(ids)):
+      user = await ctx.guild.query_members(user_ids=int(ids[j]))
+      await user[0].move_to(chn)
 
-  if msg.startswith("$list"):
-    encouragements = []
-    if "encouragements" in db.keys():
-      encouragements = db["encouragements"]
-    await message.channel.send(encouragements)
+#Voice team deleter
+@client.command()
+async def over(ctx):
+  voice_main = discord.utils.get(ctx.guild.channels, name="M3W2 Lobby")
+  if voice_main == None:
+    await ctx.send('M3W2 Lobby channel not available..!')
+    return
 
-  if msg.startswith("$responding"):
-    value = msg.split("$responding ",1)[1]
+  for c in ctx.guild.voice_channels:
+    if str(c).startswith('M3W2 Team - '):
+      memid = c.voice_states.keys()
+      memlist=list(memid)
+      if len(memlist) > 0:
+        user = await ctx.guild.query_members(user_ids=memlist)
+        for user1 in user:
+          await user1.move_to(voice_main)
+      await c.delete()
 
-    if value.lower() == "true":
-      db["responding"] = True
-      await message.channel.send("Responding is on.")
-    else:
-      db["responding"] = False
-      await message.channel.send("Responding is off.")
 
-keep_alive()
+
+#For ping
+@client.command()
+async def ping(ctx):
+  await ctx.send(f'{round(client.latency*1000)} ms')
+
+#Help Embed
+@client.command(aliases=['Help','help','HELP'])
+async def embed(ctx):
+
+  with open('prefixes.json','r') as f:
+    prefixes=json.load(f)
+    a = prefixes[str(ctx.guild.id)]
+
+  gif=random.choice(gifs2)
+
+  #### Create the initial embed object ####
+  embed=discord.Embed(title="HELP", description="These are the list of commands used in this bot", color=0xad1457)
+
+# Add author, thumbnail, fields, and footer to the embed
+  embed.set_author(name=ctx.author.display_name,  icon_url=ctx.author.avatar_url)
+
+  embed.set_thumbnail(url=gif)
+
+  embed.add_field(name=f"{a}Help", value="Display all commands of this bot and it's uses", inline=False) 
+  embed.add_field(name=f"{a}ping", value="Returns Ping of this bot", inline=True)
+  embed.add_field(name=f"{a}changeprefix", value=f"Used to change prefix.\n Syntax: {a}changeprefix [your desired prefix]", inline=False)
+
+  embed.set_footer(text="Thanks for using my Bot <3")
+
+  await ctx.send(embed=embed)
+
+
 client.run(os.environ['Token'])
