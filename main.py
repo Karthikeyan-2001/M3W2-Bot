44d2b912f857e195b6a3e954db26e952b6ca5a76
Karthikeyan J <60818223+Karthikeyan-2001@users.noreply.github.com>
2021-06-16 16:48:10 +0000

    v0.0.7 role voice

--- main.py
+++ main.py
@@ -122,11 +122,9 @@ async def vteam(ctx,notm):
   
   random.shuffle(memlist)
   memlist= np.array_split(memlist, notm)
-  await ctx.send(type(int(memlist[0][0])))
   for i in range(0,notm):
     chanT = 'M3W2 Team - '+str(i+1)
     chn = await ctx.guild.create_voice_channel(chanT, category=cat, user_limit=memlist[i].size)
-    await ctx.send(chn)
     ids=memlist[i]
     for j in range(0,len(ids)):
       user = await ctx.guild.query_members(user_ids=int(ids[j]))
@@ -136,13 +134,46 @@ async def vteam(ctx,notm):
 #Voice team deleter
 @client.command()
 async def over(ctx):
+
+  ch=ctx.author.voice
+  if ch == None:
+    await ctx.send("User not in any voice channel..!")
+    return  
+
+  autca = ctx.author.voice.channel.category
+
+  if "M3W2" in [y.name.upper() for y in ctx.author.roles]:
+    for c in ctx.guild.voice_channels:
+      if str(c).startswith('M3W2 Team - ') == False and c.category == autca:
+        ch1 = c
+
+    if ch1 == None:
+      await ctx.send('No channel exist to move Members in this category..!')
+      return
+    
+    for c in ctx.guild.voice_channels:
+      if str(c).startswith('M3W2 Team - ') and c.category == autca:
+       memid = c.voice_states.keys()
+       memlist=list(memid)
+       if len(memlist) > 0:
+          user = await ctx.guild.query_members(user_ids=memlist)
+          for user1 in user:
+            await user1.move_to(ch1)
+       await c.delete()
+       return
+
   voice_main = discord.utils.get(ctx.guild.channels, name="M3W2 Lobby")
+
+  if autca != voice_main.category:
+    await ctx.send('You\'re not in M3W2 Teams category..!')
+    return
+
   if voice_main == None:
     await ctx.send('M3W2 Lobby channel not available..!')
     return
 
   for c in ctx.guild.voice_channels:
-    if str(c).startswith('M3W2 Team - '):
+    if str(c).startswith('M3W2 Team - ') and autca == voice_main.category:
       memid = c.voice_states.keys()
       memlist=list(memid)
       if len(memlist) > 0:
@@ -150,12 +181,50 @@ async def over(ctx):
         for user1 in user:
           await user1.move_to(voice_main)
       await c.delete()
+      return
+
+  
+    
 
 
-#Voice team deleter
+    
+
+
 @client.command()
 #@commands.has_role('RoleName')
-async def over(ctx):
+async def cvteam(ctx,notm):
+  ch=ctx.author.voice
+  if ch==None:
+    await ctx.send('Connect to voice channel..!' )
+    return
+  notm = int(notm)
+  memlist=list()
+  chn1=ctx.author.voice.channel
+  memid= chn1.voice_states.keys()
+  memlist=list(memid)
+
+  if notm>len(memlist):
+    await ctx.send('Specify No. of teams less than members..!')
+    return
+
+  for c in ctx.guild.voice_channels:
+    if str(c).startswith('M3W2 Team - '):
+      memid = c.voice_states.keys()
+      memlist=list(memid)
+      if len(memlist) == 0:
+        await c.delete()
+  
+  random.shuffle(memlist)
+  memlist= np.array_split(memlist, notm)
+  for i in range(0,notm):
+    chanT = 'M3W2 Team - '+str(i+1)
+    chn = await ctx.guild.create_voice_channel(chanT, category=chn1.category, user_limit=memlist[i].size)
+    ids=memlist[i]
+    for j in range(0,len(ids)):
+      user = await ctx.guild.query_members(user_ids=int(ids[j]))
+      await user[0].move_to(chn)
+
+
 
 
 #For ping
